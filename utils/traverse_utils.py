f498049301a120d7d543a1d0af997aacae3818b7
KatKatKateryna <kateryna@speckle.systems>
2025-04-23 22:55:16 +0100

    adjust result crs

--- utils/traverse_utils.py
+++ utils/traverse_utils.py
@@ -1,4 +1,5 @@
 
+from pyproj import Transformer
 from specklepy.objects.base import Base
 
 from utils.coords_utils import reproject_display_data_list
@@ -11,6 +12,13 @@ def traverse_transform_data(commit_obj, function_inputs: "FunctionInputs") -> Ba
     wkt: str = commit_obj.crs["wkt"]
     crs_from = create_crs_from_wkt(wkt)
     crs_to = create_custom_crs(function_inputs.lat, function_inputs.lon)
+    
+    commit_obj["crs"] = {"wkt":crs_to.to_wkt(), "unit":"Meters","authid":"","description":f"SpeckleCRS_latlon_{function_inputs.lat}_{function_inputs.lon}"}
+    transformer = Transformer.from_crs(
+        crs_from,
+        crs_to,
+        always_xy=True,
+    )
 
     layers = commit_obj.elements
     for layer in layers:
@@ -23,8 +31,8 @@ def traverse_transform_data(commit_obj, function_inputs: "FunctionInputs") -> Ba
                 if is_displayable(displayData):
                     if is_primitive(displayData):
                         display_list = [displayData]
-                        reproject_display_data_list(display_list, crs_from, crs_to)
+                        reproject_display_data_list(display_list, transformer)
                     else: # Region, Brep
                         display_list = displayData.displayValue
-                        reproject_display_data_list(display_list, crs_from, crs_to)
+                        reproject_display_data_list(display_list, transformer)
 
