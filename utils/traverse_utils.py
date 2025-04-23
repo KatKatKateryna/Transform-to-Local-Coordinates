57268ee458b596e574713a6e2c49e317a56322f1
KatKatKateryna <kateryna@speckle.systems>
2025-04-23 03:03:16 +0100

    unserialized classes

--- utils/traverse_utils.py
+++ utils/traverse_utils.py
@@ -8,7 +8,7 @@ from utils.display_utils import is_displayable, is_primitive
 def traverse_transform_data(commit_obj, function_inputs: "FunctionInputs") -> Base:
     """Traverse Speckle data and transform it's coordinates."""
 
-    wkt: str = commit_obj.crs.wkt
+    wkt: str = commit_obj.crs["wkt"]
     crs_from = create_crs_from_wkt(wkt)
     crs_to = create_custom_crs(function_inputs.lat, function_inputs.lon)
 
@@ -16,7 +16,7 @@ def traverse_transform_data(commit_obj, function_inputs: "FunctionInputs") -> Ba
     for layer in layers:
         for feature in layer.elements: # each feature is a QgisObject (DataObject)
 
-            for displayData in feature.displayValue:
+            for displayData in feature["displayValue"]:
 
                 display_list = []
                 # don't change Table items - only displayable objects
@@ -24,7 +24,7 @@ def traverse_transform_data(commit_obj, function_inputs: "FunctionInputs") -> Ba
                     if is_primitive(displayData):
                         display_list = [displayData]
                         reproject_display_data_list(display_list, crs_from, crs_to)
-                    else:
+                    else: # Region, Brep
                         display_list = displayData.displayValue
                         reproject_display_data_list(display_list, crs_from, crs_to)
 
